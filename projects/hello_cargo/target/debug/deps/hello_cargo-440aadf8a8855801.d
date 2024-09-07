92cad15ac23614d5ec90b65d548b910644ad60fd
mburns7211 <mburns7211@gmail.com>
2024-09-07 00:34:46 -0400

    rust book 1.3

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d
@@ -0,0 +1,5 @@
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801: src/main.rs
+
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/hello_cargo/target/debug/hello_cargo: /Users/matt/Desktop/Development/projects/rust-the-book/projects/hello_cargo/src/main.rs
