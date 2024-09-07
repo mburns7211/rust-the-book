a7736f973232198a3b4148eab0dc3807e28d5ea4
mburns7211 <mburns7211@gmail.com>
2024-09-07 15:50:28 -0400

    rust 3.1

---
+++ projects/variables/target/debug/deps/variables-09a9eb8cb6e9989d.d
@@ -0,0 +1,5 @@
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/variables/target/debug/deps/variables-09a9eb8cb6e9989d: src/main.rs
+
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/variables/target/debug/deps/variables-09a9eb8cb6e9989d.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/matt/Desktop/Development/projects/rust-the-book/projects/variables/target/debug/variables: /Users/matt/Desktop/Development/projects/rust-the-book/projects/variables/src/main.rs
